b0a1849340aea9faf4e5122020142507f0c9a0d9
Sergey Breus <serbr2010@gmail.com>
2015-10-31 15:25:18 +0300

    Started something

--- src/Connection.java
+++ src/Connection.java
@@ -1,3 +1,78 @@
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Connection {
-    //some changes made here
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private Connection user;
+    private String nick;
+    private String myNick;
+
+    public Connection(Connection user) throws IOException {
+        serverSocket = new ServerSocket(28411);
+        socket = user.serverSocket.accept();
+
+    }
+
+    public String getNick(){
+        return nick;
+    }
+
+    public String getMyNick() {
+        return myNick;
+    }
+
+    public void sendNickHello(Connection user){
+        sendMessage("ChatApp 2015 user "+ this.getMyNick());
+    }
+
+    public void sendNickBusy(Connection user){
+        if (socket.isConnected()){
+            sendMessage("Busy");
+        }
+
+    }
+
+    void accept(){
+        sendNickHello(nick);
+    }
+
+    void reject(){
+        sendNickBusy(nick);
+        disconnect();
+    }
+
+    public void sendMessage(String message){
+        try{
+            serverSocket = new ServerSocket();
+            socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            String instr;
+            while (true){
+                out.writeUTF(message);
+                out.flush();
+                System.out.println("sending..");
+                instr= in.readUTF();
+                System.out.println(instr);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void disconnect(){
+        try{
+            socket.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
