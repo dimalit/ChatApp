2ab3f7605b762eac43ed1f710b53818b4c5c034c
maxmen <maxkrivich@gmail.com>
2015-11-01 21:24:41 +0300

    receive()  и Command

--- src/Command.java
+++ src/Command.java
@@ -1,37 +1,35 @@
 
 public class Command {
-	public static enum CommandType {
-		ACCEPT, DISCONNECT, MESSAGE, NICK, REJECT;
-		/*
-		 * public static CommandType valueOf(String s){ switch (s){ case
-		 * "Accepted":{ return ACCEPT; } case "Dicsonnect":{ return DISCONNECT;
-		 * } case "Message":{ return MESSAGE; } case "Nick":{ return NICK; }
-		 * case "Rejected":{ return REJECT; } } return null; }
-		 */
-	}
 
-	public void sendNickHello(String nick) {
-		// TODO
-	}
+    public Command.CommandType type;
 
-	public void sendNickBusy(String nick) {
-		// TODO
-	}
+    public static enum CommandType {ACCEPT, DISCONNECT, MESSAGE, NICK, REJECT;}
 
-	public void accept() {
+    public Command(Command.CommandType t) {
+        type = t;
+    }
 
-	}
+    public void sendNickHello(String nick) {
+        // TODO
+    }
 
-	public void reject() {
+    public void sendNickBusy(String nick) {
+        // TODO
+    }
 
-	}
+    public void accept() {
 
-	public void sendMessage(String Message) {
+    }
 
-	}
+    public void reject() {
 
-	public void disconnect() {
+    }
 
-	}
+    public void sendMessage(String Message) {
 
+    }
+
+    public void disconnect() {
+
+    }
 }
