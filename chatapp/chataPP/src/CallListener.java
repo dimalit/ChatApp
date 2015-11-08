bb213f1d0be595ea838f8c579b1be531fd3bf601
Серёга <@Asus>
2015-11-08 22:37:38 +0200

    new version CallListener.java

--- chatapp/chataPP/src/CallListener.java
+++ chatapp/chataPP/src/CallListener.java
@@ -6,31 +6,14 @@ import java.util.logging.Logger;
 public class CallListener
 {
 
-    private ServerSocket s;
-    private final int port = 9999;
-    private Socket fromclient;
-    
-    public CallListener()
+    public CallListener() 
     {
-      try {
-            s = new ServerSocket(port);
-        } catch (IOException ex) {
-            Logger.getLogger(Caller.class.getName()).log(Level.SEVERE, null, ex);         
-        }   
         
     }
-     
+
+  
     
-        public void Wait()
-    {
-        System.out.println("Start...");
-        try {
-            fromclient = s.accept();
-        } catch (IOException ex) {
-            Logger.getLogger(Caller.class.getName()).log(Level.SEVERE, null, ex);
-        }
-       System.out.println("");  
-    }   
+    
     
     
 }
