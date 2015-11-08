8c7d355a1d9d5b265cdc2fd8c9f8a11e19fdf8e8
Серёга <@Asus>
2015-11-08 22:23:11 +0200

    add variable and methods

--- chatapp/chataPP/src/Caller.java
+++ chatapp/chataPP/src/Caller.java
@@ -2,14 +2,35 @@ import java.net.*;
 import java.io.*;
 public class Caller 
 {
-
-    public Caller() 
-    {
-        
-        
-    }
- 
+    private static final int remotePort = 9999;
+    private Socket socket;
+    private String localNick;
+    private String remoteNick;
+    private String remoteAdress;
     
+    
+    public Caller() {
+    }
+
+    public void Call() throws UnknownHostException, IOException {
+		socket = new Socket(remoteAdress, remotePort);
+	}
+
+	public void setLocalNick(String localNick) {
+		this.localNick = localNick;
+	}
+
+	public void setRemoteNick(String remoteNick) {
+		this.remoteNick = remoteNick;
+	}
+
+	public void setRemoteAdress(String remoteAdress) {
+		this.remoteAdress = remoteAdress;
+	}
+	
+	public Socket getSocket(){
+		return socket;
+	}
     
     
     
