371ba114cfc6cfcd62ca29bc75d08aee0f835f82
Sanches007 <perederin96@mail.ru>
2015-11-08 22:36:21 +0200

    Comand next levl

--- chatapp/chataPP/src/Comand.java
+++ chatapp/chataPP/src/Comand.java
@@ -1,12 +1,19 @@
 
-
-/**
- *
- * @author САНЯ
- */
 public class Comand {
-    public Comand(){
-        
-    }
-    
+	CommandType commandType;
+
+	public Comand(CommandType commandType) {
+		this.commandType = commandType;
+	}
+
+	public String toString() {
+		return commandType.toString();
+	}
+
+	public static enum CommandType {
+		ACCEPT, DISCONNECT, MESSAGE, NICK, REJECT;
+
+
+
+	}
 }
