049620ea21e600e37f0a914a4489bcdca69ac0cb
Sanches007 <perederin96@mail.ru>
2015-11-11 22:21:48 +0200

    Mes

--- chatapp/chataPP/src/MessageCommand.java
+++ chatapp/chataPP/src/MessageCommand.java
@@ -1,4 +1,13 @@
+public class MessageCommand extends Command {
+	String message;
 
-public class MessageCommand {
-    
+	public MessageCommand(String message) {
+		super(Command.CommandType.MESSAGE);
+		this.message = message;
+	}
+
+	public String toString() {
+		return message + "\n";
+	}
+
 }
