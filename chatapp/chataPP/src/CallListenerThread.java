4e73a84c33f20f3eda4dd0bb4661168c097316bf
Серёга <@Asus>
2015-11-17 20:23:31 +0200

    fix class

--- chatapp/chataPP/src/CallListenerThread.java
+++ chatapp/chataPP/src/CallListenerThread.java
@@ -1,14 +1,65 @@
-
-public class CallListenerThread implements Runnable
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.util.Observable;
+public class CallListenerThread extends Observable implements Runnable
 {
 
-    
-    
-    @Override
-    public void run() {
-        
+private CallListener listener;
+private boolean isAvailable;
+public CallListenerThread() {
+		listener = new CallListener();
+		start();
+	}
+public CallListenerThread(String localNick) {
+		listener = new CallListener(localNick);
+		start();
+	}
+public CallListenerThread(String localNick, String localIP) {
+		listener = new CallListener(localNick, localIP);
+		start();
+	}
+public String getLocalNick() {
+return listener.getLocalNick();
+	}
+public String getRemoteNick() {
+return listener.getRemoteNick();
+	}
+public SocketAddress getListenAddress() throws IOException {
+return listener.getListenAddress();
+	}
+public SocketAddress getRemoteAddress() throws IOException {
+return listener.getRemoteAddress();
+	}
+public boolean isBusy() {
+return listener.isStatusBusy();
+	}
+public void setBusy(boolean busy) {
+		listener.setStatus(busy);
+	}
+public void setLocalNick(String localNick) {
+		listener.setLocalNick(localNick);
+	}
+public void setListenAddress(SocketAddress listenAddress){
+//TODO
     }
- 
+public void run() {
+while (true) {
+try {
+				listener.getConnection();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			setChanged();
+			notifyObservers();
+		}
+	}
+public void start() {
+this.isAvailable = true;
+		run();
+	}
+public void stop() {
+this.isAvailable = false;
+	}
     
     
 }
