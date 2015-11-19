206f979f916d16b373cb7efb15fe649fc677d516
Sergey Artyuh <metalgearrising8435@gmail.com>
2015-11-20 00:19:02 +0200

    delete

--- chatapp/chataPP/src/MessageCommand.java
+++ chatapp/chataPP/src/MessageCommand.java
@@ -1,13 +1,4 @@
 public class MessageCommand extends Command {
-	String message;
-
-	public MessageCommand(String message) {
-		super(Command.CommandType.MESSAGE);
-		this.message = message;
-	}
-
-	public String toString() {
-		return message + "\n";
-	}
+	
 
 }
