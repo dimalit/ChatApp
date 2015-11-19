d927ac0b20684115a1c4032016007b58ba47f600
Sergey Artyuh <metalgearrising8435@gmail.com>
2015-11-20 00:19:10 +0200

    add

--- chatapp/chataPP/src/MessageCommand.java
+++ chatapp/chataPP/src/MessageCommand.java
@@ -1,4 +1,13 @@
 public class MessageCommand extends Command {
-	
+	   private String message;
+
+	    public MessageCommand(String message) {
+	        super(Command.CommandType.valueOf("MESSAGE"));
+	        this.message = message;
+	    }
+
+	    public String toString (){
+	        return message;
+	    }
 
 }
