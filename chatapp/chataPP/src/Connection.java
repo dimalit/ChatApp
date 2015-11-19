694ed6f856576d662cbb887921aa9012ee6207ba
Sergey Artyuh <metalgearrising8435@gmail.com>
2015-11-20 00:24:30 +0200

    fix

--- chatapp/chataPP/src/Connection.java
+++ chatapp/chataPP/src/Connection.java
@@ -3,106 +3,40 @@ import java.net.*;
 
 public class Connection {
   
-    private final int port = 28411;
-	private OutputStream os;
-	public Socket socket;
-	private String nick;
-	private DataOutputStream dos;
-	private DataInputStream in;
+    private Socket socket;
 
-	public void disconnect() throws IOException {
-		dos.write("DISCONNECT\n".getBytes());
-		dos.flush();
-		dos.close();
-		socket.close();
-	}
+    public Connection(Socket socet) {
+        this.socket = socet;
+    }
 
-	public Connection(String IP, String Nick) {
-		try {
-			socket = new Socket(InetAddress.getByName(IP), port);
-			os = socket.getOutputStream();
-			dos = new DataOutputStream(os);
-			in = new DataInputStream(socket.getInputStream());
-			//m.setTo(IP);
-			//m.setFrom(socket.getInetAddress().getHostName());
-			nick = Nick;
-			System.out.println(IP + " is connected. Type message.");
-		} catch (IOException ex) {
-			System.out.println("Bad");
-		}
-	}
-	
-	public Connection(Socket s, String Nick) {
-		try {
-			socket = s;
-			os = socket.getOutputStream();
-			dos = new DataOutputStream(os);
-			in = new DataInputStream(socket.getInputStream());
-			nick = Nick;
-			System.out.println(s.getInetAddress() + " is connected. Type message.");
-		} catch (IOException ex) {
-			System.out.println(ex);
-		}
-	}
-	
-	public boolean isOpen(){
-		return !socket.isClosed();
-		}
+    public void sendNickHello(String nick) throws IOException {
+        socket.getOutputStream().write((Constants.ChatApp_VERSION + " user " + nick + "\n").getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 
-	public void accept() throws IOException {
-		dos.write("ACCEPT\n".getBytes());
-		dos.flush();
-	}
+    public void sendNickBusy(String nick) throws IOException {
+        socket.getOutputStream().write((Constants.ChatApp_VERSION + " user " + nick + " busy\n").getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 
-	public void reject() throws IOException {
-		dos.write("REJECTED\n".getBytes());
-		dos.flush();
-	}
+    public void accept() throws IOException {
+        socket.getOutputStream().write("Accepted\n".getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 
-	public void sendNickHello(String nick1) throws IOException {
-		dos.write(("ChatApp 2015 " + nick+ "\n").getBytes());
-		dos.flush();
-	}
+    public void reject() throws IOException {
+        socket.getOutputStream().write("Rejected\n".getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 
-	public void sendNickBusy(String nick1) throws IOException {
-		dos.write(("ChatApp 2015 " + nick + " busy" + "\n").getBytes());
-		dos.flush();
-	}
+    public void sendMessage(String message) throws IOException {
+        socket.getOutputStream().write("Message\n".getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().write((message + "\n").getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 
-	public void sendMessage(String text) throws IOException {
-		dos.write("MESSAGE\n".getBytes());
-		dos.write(text.getBytes());
-		dos.flush();
-	}
-
-//	public Command receive() throws IOException {
-//		String command = "";
-//		Command comand;
-//		int n;
-//		while (true) {
-//			if((n = in.read()) == '\n') {
-//					break;
-//			} else
-//				command += (char) n;
-//		}
-//		comand = new Command(command.toUpperCase());
-//		if (command.startsWith("ChatApp")) {
-//			//command for nickname
-//			comand = new Command("Nick");
-//		}
-//		
-//		if(command.toUpperCase().equals("MESSAGE")) {
-//            String message = "";
-//            while (true) {
-//                if((n = in.read()) == '\n')
-//                    break;
-//                else
-//                	message += (char) n;
-//            }
-//            //command for message
-//		}
-//		return comand;
-//	}
-//    
-    
+    public void disconnect() throws IOException {
+        socket.getOutputStream().write("Disconnect\n".getBytes(Constants.CHARSET_NAME));
+        socket.getOutputStream().flush();
+    }
 }
