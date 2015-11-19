b18a59c4761ed8826baa9787c9e6ed2854c8427d
Sergey Artyuh <metalgearrising8435@gmail.com>
2015-11-20 00:28:08 +0200

    delete

--- chatapp/chataPP/src/CommandListenerThread.java
+++ chatapp/chataPP/src/CommandListenerThread.java
@@ -2,61 +2,10 @@
 import java.io.IOException;
 import java.util.Observable;
 
-public class CommandListenerThread extends Observable implements Runnable {
-	private boolean stopFlag;
-	private boolean disconnect;
-	private Connection connection;
-	private Command lastCommand;
+public class CommandListenerThread implements Runnable {
+	
+   
 
-	CommandListenerThread() {
-
-	}
-
-	CommandListenerThread(Connection connection) {
-
-	}
-
-	Command getLastCommand() {
-		return lastCommand;
-	}
-
-
-	public void run() {
-		while (!disconnect) {
-
-			while (!disconnect) {
-
-				try {
-					synchronized (this) {
-						this.lastCommand = connection.receive();
-                                                    if (lastCommand != null)
-                                                        if ((lastCommand.commandType == (Command.CommandType.DISCONNECT)
-								|| (lastCommand.commandType == (Command.CommandType.REJECT)))) {
-							disconnect = true;
-							System.out.println("test");
-						}
-						setChanged();
-						notifyObservers();
-					}
-				} catch (IOException e) {
-
-					e.printStackTrace();
-
-				}
-			}
-		}
-
-	}
-
-	void start() {
-		disconnect = false;
-		Thread t = new Thread();
-		t.start();
-	}
-
-	void stop() {
-		disconnect = true;
-
-	}
-
+    
+    
 }
