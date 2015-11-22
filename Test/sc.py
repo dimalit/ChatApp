dc6b193965a8778317b8b7ff547e14f4d85cfc3b
Max Krivich <maxkrivich@users.noreply.github.com>
2015-11-22 12:07:49 +0300

    Create sc.py

---
+++ Test/sc.py
@@ -0,0 +1,9 @@
+import socket
+s = socket.socket()
+f = open('command.txt','r')
+s.connect((str(input()), 28411))
+cmd = f.readline()
+while cmd != '':
+    s.send(cmd.encode())
+    cmd = f.readline()
+s.close()
