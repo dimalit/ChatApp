878e8ede29ec4fb0b23728c8ae145d1def629e62
Серёга <@Desktop>
2015-11-25 17:06:06 +0200

    add class const v 1.0

---
+++ chatapp/chataPP/src/Const.java
@@ -0,0 +1,8 @@
+
+public final class Const 
+{
+ static final String CHARSET_NAME = "UTF-8";    
+ public static final int PORT = 28411;
+ static final String ChatApp_VERSION = "ChatApp 2015";
+   
+}
