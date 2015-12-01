b2f6ec7072cec1516c1e4a7989e0cdf8cca665e0
Ivan Kudryashov <entick@yandex.ru>
2015-12-01 15:02:19 +0200

    Add some features in contactsView

--- src/ContactsView.java
+++ src/ContactsView.java
@@ -10,9 +10,14 @@ ContactsView()
 ContactsView(ServerConnection server)
 {
 	for (int i=0;i<server. getAllNicks().length;i++)
-	{model=new ContactsModel(server. getAllNicks()[i],server.isNickOnline(server.getAllNicks()[i])) ;
+	{model=new ContactsModel(server.getAllNicks()[i],server.isNickOnline(server.getAllNicks()[i])) ;
+	try{
 	str[i]=model.toString();
-	addElement(model.toString());
+	addElement(model.toString());}
+	catch(NullPointerException e){
+		System.out.println("no one user online on server");
+		break;
+	}
 			
 	}
 	
