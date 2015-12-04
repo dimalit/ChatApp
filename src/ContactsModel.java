a75260e69cada07b9141b0cfd5a96ac8259d247e
Max Krivich <maxkrivich@users.noreply.github.com>
2015-12-04 20:58:02 +0300

    Update ContactsModel.java

--- src/ContactsModel.java
+++ src/ContactsModel.java
@@ -1,28 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-public class ContactsModel {
-	private String nick;
-	private boolean isOnline;
-	ContactsModel(String nick,boolean online)
-	{this.nick=nick;
-	this.isOnline=online;
+	import java.util.ArrayList;
+	import java.util.List;
+	
+	public class ContactsModel {
+		private String nick;
+		private boolean isOnline;
+	
+		ContactsModel(String nick, boolean online) {
+			this.nick = nick;
+			this.isOnline = online;
+		}
+	
+		public String getNick() {
+			return nick;
+		}
+	
+		public boolean isOnline() {
+			return isOnline;
+		}
+	
+		public String toString() {
+			if (isOnline)
+				return nick + " online";
+			return nick;
+		}
 	}
-	public String getNick()
-	{
-		return nick;
-	}
-	public boolean isOnline()
-	{
-		return isOnline;
-	}
-	public String toString()
-	{
-	if (isOnline())
-		return getNick()+" online";
-	else
-		return getNick();
-	}
-}
