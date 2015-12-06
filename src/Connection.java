54017d1fc956d27641dc3d57b725bcc5676c5c25
Jojorr <mr.rjkov96@gmail.com>
2015-12-06 21:24:04 +0200

    still fixing

--- src/Connection.java
+++ src/Connection.java
@@ -5,53 +5,53 @@ import java.util.Scanner;
 public class Connection{
     private Socket socket;
     private OutputStream out;
-    private PrintWriter sout;
+    private DataOutputStream sout;
     private BufferedReader reader;
 
     public Connection(Socket s) throws IOException{
         this.socket = s;
         out = socket.getOutputStream();
-        sout = new PrintWriter(out,true);
+        sout = new DataOutputStream(socket.getOutputStream());
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
     }
 
     public void sendMessage(String message) throws IOException{
-        sout.print("Message\n"+message+"\n");
+        sout.write(new StringBuilder("Message\n").append(message).append("\n").toString().getBytes());
         sout.flush();
     }
 
     public void disconnect() throws IOException{
-        sout.print("Disconnect\n");
+        sout.write(new StringBuilder("Disconnect\n").toString().getBytes());
         out.close();
         reader.close();
         socket.close();
     }
 
     public void sendNickHello(String nick) throws IOException {
-        sout.print("ChatApp 2015 user "+nick+"\n");
+        sout.write(new StringBuilder("ChatApp 2015 user "+nick+"\n").toString().getBytes());
         sout.flush();
     }
 
     public void sendNickBusy(String nick) throws IOException {
         if(socket.isConnected()){
-            sout.print("ChatApp 2015 user "+nick+" busy\n");
+            sout.write(new StringBuilder("ChatApp 2015 user ").append(nick).append(" busy\n").toString().getBytes());
             sout.flush();
         }
     }
 
     public void accept() throws IOException{
-        sout.print("Accepted\n");
+        sout.write(new StringBuilder("Accepted\n").toString().getBytes());
         sout.flush();
     }
 
     public void reject() throws IOException {
-        sout.print("Rejected\n");
+        sout.write(new StringBuilder("Rejected\n").toString().getBytes());
         sout.flush();
     }
 
     public Command recieve() throws IOException {
        // BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-        String s= reader.readLine();
+        String s = reader.readLine();
         if(s.contains("Message")){
             s+=reader.readLine();
         }
