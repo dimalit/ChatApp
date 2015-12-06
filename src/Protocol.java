437783e84018abc29ddfbf022a99408919db2001
Nicholas <prototype_igrick@mail.ru>
2015-12-06 22:06:08 +0200

    *

--- src/Protocol.java
+++
@@ -1,7 +0,0 @@
-
-public class Protocol {
-    public static final int PORT =28411;
-    public static boolean isBusy;
-    public static String localNick = "default";
-    public static String remoteNick;
-}
