4e448e436b7a1f0cf3cf05df10001e45c1beeb2c
AnastasiaKiyan <nastya93-1@Mail.ru>
2015-12-11 19:48:48 +0200

    Update ContactsView.java

--- src/ContactsView.java
+++ src/ContactsView.java
@@ -1,29 +1,47 @@
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.DefaultListModel;
 
-public class ContactsView extends DefaultListModel {
+public class ContactsView extends DefaultListModel  {
 	private String[] str;
 	private ContactsModel model;
+	private List<ContactsModel> SCont;
+	private ContactsModel m;
+private ServerConnection server;
 
-	ContactsView() {
+	ContactsView(ServerConnection server) throws IOException {
+		this.server=server;
+		SCont=new ArrayList<ContactsModel>();
+		addServerNicks();
 
 	}
-
-	ContactsView(ServerConnection server) {
-		String arr[] = server.getAllNicks();
-		str = new String[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			model = new ContactsModel(arr[i], server.isNickOnline(arr[i]));
-			try {
-				str[i] = model.toString();
-				addElement(model.toString());
-			} catch (NullPointerException e) {
-				System.out.println("no one user online on server");
-				break;
-			}
-
+public void addServerNicks()
+{
+	String arr[] = server.getAllNicks();
+	str = new String[arr.length];
+	for (int i = 0; i < arr.length; i++) {
+		model = new ContactsModel(arr[i], server.isNickOnline(arr[i]));
+		try {
+			addElement(model.toString());
+		} catch (NullPointerException e) {
+			System.out.println("no one user online on server");
+			break;
 		}
 
 	}
+
+}
+
 
 	String[] getStr() {
 		return str;
